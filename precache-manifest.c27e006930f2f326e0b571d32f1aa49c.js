723a41342b2c545b79e945cba1b349fb6dba3ba1
SongYongsoo <syssys131@naver.com>
2020-08-24 22:49:52 +0900

    Updates

---
+++ precache-manifest.c27e006930f2f326e0b571d32f1aa49c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f10713ccb47cf2531719b1463e2842",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "062efcbc80d4bbe75f64",
+    "url": "/movie_app_2019/static/css/main.fecc8da0.chunk.css"
+  },
+  {
+    "revision": "4d7c806d0e258b226914",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062efcbc80d4bbe75f64",
+    "url": "/movie_app_2019/static/js/main.5af58077.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
